f40772287282857bc4c2d5ae79e59ef28ac06279
David M <elarreglador@protonmail.com>
2024-03-24 18:59:02 +0100

    Comprtamiento del FormField

--- lib/presentation/screens/shared/message_field_box.dart
+++ lib/presentation/screens/shared/message_field_box.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MessageFieldBox extends StatelessWidget {
   const MessageFieldBox({super.key});
   
   @override
   Widget build(BuildContext context) {
+
+    final textControll = TextEditingController();
+
+    final focusNode = FocusNode();
 
     final colors = Theme.of(context).colorScheme; // color del tema actual
 
@@ -14,25 +19,32 @@ class MessageFieldBox extends StatelessWidget {
     );
 
     final inputDecoration = InputDecoration( // decoracion del campo de texto
-        enabledBorder: outlineInputBorder,
-        focusedBorder: outlineInputBorder,
-        filled: true,
-        suffixIcon: IconButton(
-          onPressed: () { 
-            print ("Valor de la caja de texto: ");
-          },
-          icon: const Icon (Icons.send_outlined),
-        )
-      );
+      hintText: "Finaliza tu mensaje con un '?'",
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        onPressed: () { 
+          final textValue = textControll.value.text;
+          print ("Valor de la caja de texto: $textValue");
+          textControll.clear();
+        },
+        icon: const Icon (Icons.send_outlined),
+      )
+    );
 
     // CAMPO DE TEXTO
     return TextFormField(
+      onTapOutside: (event){ //retira el foco si clicamos fuera
+        focusNode.unfocus();
+      },
+      focusNode: focusNode, //tiene el foco
+      controller: textControll,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
         print("Submit value: $value");
-      },
-      onChanged: (value) {
-        print("Changed: $value");
+        textControll.clear();
+        focusNode.requestFocus(); //recupera el foco tras enviar mensaje
       },
     );
   }
