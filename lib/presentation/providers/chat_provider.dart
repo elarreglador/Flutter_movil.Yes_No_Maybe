194ccb7491cd3cc7f3accf84fc4188bb7396141d
David M <elarreglador@protonmail.com>
2024-03-25 16:38:15 +0100

    Mover el scroll al final en cada msg enviado

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -3,6 +3,9 @@ import 'package:ud5_yes_no_maybe/domain/entities/message.dart';
 
 
 class ChatProvider extends ChangeNotifier{
+
+  final ScrollController chatScrollController = ScrollController();
+
   // LISTA DE MENSAJES QUE APARECEN EN EL CHAT
   List <Message> messageList = [
     Message(text: "hola mundo", fromWho: FromWho.me),
@@ -10,11 +13,25 @@ class ChatProvider extends ChangeNotifier{
     Message(text: "aun no?", fromWho: FromWho.me)
   ];
 
-  
+  // Funcion que agrega mensajes a la lista
   Future<void> sendMessage (String text) async{
+
+    if (text.isEmpty) return;
+
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messageList.add(newMessage);
 
     notifyListeners();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      moveScrollToBottom();
+    });
+  }
+
+  void moveScrollToBottom(){
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent, 
+      duration: const Duration ( milliseconds: 300), 
+      curve: Curves.easeOut
+    );
   }
 }
--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -54,6 +54,7 @@ class ChatView extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
+                controller: chatProvider.chatScrollController,
                 itemCount: chatProvider.messageList.length,
                 itemBuilder: (context, index) {
                   final message = chatProvider.messageList[index];
