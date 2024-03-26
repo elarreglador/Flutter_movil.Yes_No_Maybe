74694fc5486c1d70a650f9aa67488b940d6a3e5c
David M <elarreglador@protonmail.com>
2024-03-26 07:30:47 +0100

    creado yesNoModel.toMessageEntity()

--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -1,4 +1,5 @@
 class YesNoModel {
+
   String answer;
   bool forced;
   String image;
@@ -7,12 +8,17 @@ class YesNoModel {
     required this.answer,
     required this.forced,
     required this.image
-  })
+  });
 
-  factory YesNoModel.fromJsonMap(Map<String,dynamic> json) =>
-  YesNoModel(
+  factory YesNoModel.fromJsonMap(Map<String,dynamic> json) => YesNoModel(
     answer: json['answer'], 
     forced: json['forced'], 
     image: json['image']
   );
+
+  Map<String, dynamic> toJson() => {
+    "answer": answer,
+    "forced": forced,
+    "image": image
+  };
 }
